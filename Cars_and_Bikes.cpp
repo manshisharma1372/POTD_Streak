aa4a27679b32d6f42fbc3f71f7395ecbbfd2bdf7
manshisharma1372 <manshisharma1372@gmail.com>
2022-04-13 00:49:10 +0530

    .\Cars_and_Bikes.cpp

---
+++ Cars_and_Bikes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x%4==0)
+        cout<<"NO"<<endl;
+	    else
+         cout<<"YES"<<endl;
+	}
+	return 0;
+}
