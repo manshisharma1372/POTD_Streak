6658891928500f15319262b555168700b96c8335
manshisharma1372 <76405335+manshisharma1372@users.noreply.github.com>
2022-11-08 01:59:39 +0530

    Added solution - LeetHub

--- Largest Element in Array - GFG/largest-element-in-array.cpp
+++ Largest Element in Array - GFG/largest-element-in-array.cpp
@@ -12,13 +12,8 @@ class Solution
 public:
     int largest(vector<int> &arr, int n)
     {
-        int max=arr[0];
-        for(int i=0;i<n;i++){
-            if(arr[i]>max){
-                max=arr[i];
-            }
-        }
-        return max;
+        sort(arr.begin(),arr.end());
+        return arr[arr.size()-1];
     }
 };
 
