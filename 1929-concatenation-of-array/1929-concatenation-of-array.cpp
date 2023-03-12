617764787cd578eb5ae1b9b3218a9adf2656411e
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-03-12 22:37:37 +0530

    Time: 9 ms (63.28%), Space: 12.9 MB (23.74%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -20,7 +20,8 @@ public:
         //withput extra space 
         long long n=nums.size();
         for(int i=0;i<n;i++){
-            nums.push_back(nums[i]);
+            
+         nums.push_back(nums[i]);
         }
         return nums;
         
