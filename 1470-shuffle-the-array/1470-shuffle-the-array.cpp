0e8c1380beb5d37db2a62c545c5f0a31f8550220
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-03-14 19:37:25 +0530

    Time: 3 ms (91.97%), Space: 9.6 MB (97.00%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
+        
+        //extra space--->my soln
+        /*
         int size=nums.size();
         vector<int> x;
         vector<int> y;
@@ -18,7 +21,23 @@ public:
             ans.push_back(y[i]);
         }
         return ans;
+        */
         
+        //o(1)
+          int first = 0, second = n, max = 1001;
+        //max is 1+10^3(given)
+        
+        int size=nums.size();
+        for(int i=0;i<size;i++)
+        {
+            if(i%2==0) //even
+            nums[i]=(nums[first++]%max)*max + nums[i];
+            else
+            nums[i]=(nums[second++]%max)*max + nums[i];
+        }
+        for(int i=0;i<size;i++)
+        nums[i]/=max;
+        return nums;
         
     }
 };
