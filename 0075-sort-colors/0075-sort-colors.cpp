c9bb2bab2161d0221c1390ba6d5ff75266e296b4
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-03-18 12:11:56 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (31.60%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,8 +1,41 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-         sort(nums.begin(),nums.end());
         
+        // @method 1 cannot use
+         //sort(nums.begin(),nums.end());
+        
+        
+        
+        //@method 2 counting method
+        
+        int count_zero=0;
+        int count_ones=0;
+        int count_two=0;
+        
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                count_zero++;
+            }
+            
+            else if(nums[i]==1){
+                count_ones++;
+            }
+            else if(nums[i]==2){
+                count_two++;
+            }
+            
+        }
+        
+        for(int i=0;i<count_zero;i++){
+            nums[i]=0;
+        }
+         for(int i=count_zero;i<count_zero+count_ones;i++){
+            nums[i]=1;
+        }
+         for(int i=count_zero+count_ones;i<count_zero+count_ones+count_two;i++){
+            nums[i]=2;
+        }
         
     }
 };
