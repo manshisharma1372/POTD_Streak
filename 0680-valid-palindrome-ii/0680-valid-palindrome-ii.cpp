6d0b826879ca862f07f9e127868026aa15e245b1
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-03-20 19:57:38 +0530

    Time: 56 ms (50.20%), Space: 23.9 MB (57.46%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    bool checkPalindrome(string s,int i,int j){
+      bool checkPalindrome(string s,int i,int j){
         while(i<=j){
             if(s[i]!=s[j])
              return false;
