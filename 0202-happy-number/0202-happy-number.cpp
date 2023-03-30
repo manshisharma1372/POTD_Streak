1ed949ed63ccacb18cc3a9c67b7413cf18505454
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-03-31 01:29:53 +0530

    Time: 3 ms (53.37%), Space: 6.4 MB (27.78%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    int digitSqSum(int n){
-        int sum=0;
-        while(n>0){
-            sum=sum+((n%10)*(n%10));
-            n=n/10;
-        }
-        return sum;
-    }
-    
-    bool isHappy(int n) {        
-        int temp=n;
-        while(1)
-        {
-            if(temp==89)
-                return false;
-            if(temp==1)
-                return true;
-            temp=digitSqSum(temp);
-        }
+    bool isHappy(int n) {
         
+        set<int> s;
+        
+        while(1){
+            
+            int sum=0;
+            while(n!=0){
+                
+                int last_digit=n%10;
+                sum+=pow(last_digit,2);
+                n=n/10;
+            }
+            
+            if(sum==1){
+                return true;
+            }
+            
+            if(s.find(sum)!=s.end()){
+                return false;
+            }
+            
+            s.insert(sum);
+            n=sum;
+        }
     }
 };
