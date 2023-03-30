db91eeb1111795b17484383b5ec1178302609fd8
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-03-31 00:50:26 +0530

    Time: 4 ms (71.89%), Space: 10.2 MB (87.46%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-         int countNeg = 0;
-        for(auto x: nums){
-            if(x < 0)
-                countNeg++;
-            else if(x == 0)
+        int c=0;
+        for(auto it:nums){
+            if(it<0){
+                c++;
+            }
+            
+            else if(it==0){
                 return 0;
+            }
         }
-        if(countNeg%2 ==0)
+        
+        if(c%2==0){
             return 1;
-        return -1;
+        }
+        else
+            return -1;
+        
     }
 };
