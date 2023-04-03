869c8ee5e0586b3742852223ac77a9a98a4463b0
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-04-03 22:02:49 +0530

    Time: 4 ms (57.93%), Space: 6.6 MB (75.83%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         
+        
+        //using map
+        /*
         char ans;
         map<char,int> m;
         for(auto &it:s){
@@ -17,5 +20,14 @@ public:
         }
       
         return ans;
+        */
+        
+        //using sort
+       int n = s.length();
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for(int i=0; i<n; i++) 
+            if(s[i]!=t[i]) return t[i];
+        return t[n];
     }
 };
