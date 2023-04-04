106fc68e6c2ed4e9c89470b25987d278b4d88c0f
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-04-04 18:18:35 +0530

    Time: 4 ms (39.46%), Space: 6.2 MB (13.17%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -2,10 +2,28 @@ class Solution {
 public:
     string toLowerCase(string s) {
         
+        
+        //using lib func
+        /*
         string ans;
         for(int i=0;i<s.length();i++){
             ans.push_back(tolower(s[i]));
         }
         return ans;
-    }
+          }
+        */
+
+        
+        
+        
+        
+        //without func
+        for(auto&it:s){
+            if(it>='A' && it<='Z'){
+                it=it+'a'-'A';
+            }
+        }
+        return s;
+        }
+    
 };
