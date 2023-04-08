35e335f12f1753e03d96172c1c84d075671e72a0
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-04-09 01:20:47 +0530

    Time: 18 ms (15.03%), Space: 6 MB (21.29%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int fib(int n) {
+        
+        /*
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        int fib[n+1];
+        
+        
+            fib[0]=0;
+            fib[1]=1;
+            for(int i=2;i<=n;i++){
+                fib[i]=fib[i-1]+fib[i-2];
+            }
+              return fib[n];
+              */
+        
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        int ans=fib(n-2)+fib(n-1);
+        return ans;
+        
+    }
+};
