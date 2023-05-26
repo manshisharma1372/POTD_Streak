fe50172e3fb953520f04edb2fa1221c5ee32ddc1
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-05-27 01:59:44 +0530

    Time: 567 ms (37.18%), Space: 120.8 MB (17.95%) - LeetHub

--- 2227-encrypt-and-decrypt-strings/2227-encrypt-and-decrypt-strings.cpp
+++ 2227-encrypt-and-decrypt-strings/2227-encrypt-and-decrypt-strings.cpp
@@ -1,5 +1,5 @@
 
-/*class Encrypter {
+class Encrypter {
 public:
    
     map<char,string> m;
@@ -16,7 +16,7 @@ public:
     string encrypt(string s) {
         string ans="";
        for(int i=0;i<s.size();i++) {
-        if(m.count(s[i]==0)){
+        if(m.count(s[i])==0){
             return " ";
         }
         ans+=m[s[i]];
@@ -39,7 +39,6 @@ public:
     }
 };
 
-*/
 
 
 
@@ -51,56 +50,7 @@ public:
 
 
 
-class Encrypter {
-public:
 
-    map<char, string>m;
-    vector<string>d;
-
-    Encrypter(vector<char>& keys, vector<string>& values, vector<string>& dictionary) {
-
-
-        //key---->a,b,c,d
-        //value--->ei,zf,ei,am
-
-        //m[a]=ei
-        //m[b]=zf
-        //m[c]=ei
-        //m[d]=am
-
-        for (int i = 0; i < keys.size(); i++) {
-            m[keys[i]] = values[i];
-        }
-        d = dictionary;
-    }
-
-    string encrypt(string s) {
-        string ans = "";
-
-        for (int i = 0; i < s.size(); i++) {
-            if (m.count(s[i]) == 0) {
-                return "";
-            }
-            ans += m[s[i]];
-        }
-        return ans;
-    }
-
-    int decrypt(string word2) {
-
-        int ans = 0;
-        for (int i = 0; i < d.size(); i++) {
-            //i=0; d[0]==string is string ko encrypt karoge and then check karoge word2 ke sath
-
-            string s = encrypt(d[i]);
-            if (s == word2) {
-                ans++;
-            }
-        }
-
-        return ans;
-    }
-};
     
 /**
  * Your Encrypter object will be instantiated and called as such:
