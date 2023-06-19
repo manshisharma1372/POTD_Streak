b9d00be3c416917a30ffdc78468c98fed1a04b72
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-06-19 23:35:42 +0530

    Time: 129 ms, Memory: 61.1 MB - LeetHub

---
+++ find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        
+        
+        //o(n log n)
+        /*
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                return nums[i];
+            }
+        }
+        
+        return -1;
+        
+        */
+        
+        
+        //visited soln tc=o(n)  sc=o(1) but array is modified
+        
+        /*
+        
+        int ans=-1;
+        for(int i=0;i<nums.size();i++){
+            
+            int index=abs(nums[i]);
+            
+            //already visited?
+            if(nums[index]<0){
+                ans=index;
+                break;
+            }
+            
+            //mark it visited
+            nums[index]*=-1;
+            
+        }
+        return ans;
+        */
+        
+//positoning method
+        
+        while(nums[0]!=nums[nums[0]]){
+            swap(nums[0],nums[nums[0]]);
+        }
+        
+        return nums[0];
+        
+    }
+};
