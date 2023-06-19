6b42b2a64bca045577911b91e8b9144829d7212d
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-06-19 23:14:30 +0530

    Time: 4 ms (76.98%), Space: 9 MB (10.92%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    
     int findPeakIndex(vector<int> arr){
         int s=0;
         int e=arr.size()-1;
         int mid=s+(e-s)/2;
         
-        while(s<e){
-            
+        while(s<e){  
+            //we are i line a
             if(arr[mid]<arr[mid+1]){
-                
                 //right search
-                
                 s=mid+1;
             }
             else{
