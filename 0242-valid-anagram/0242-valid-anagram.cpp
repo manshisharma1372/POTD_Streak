02252c6feb09a006cd18f52b3263158baa34393c
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-06-21 00:45:28 +0530

    Time: 4 ms (95.63%), Space: 7.3 MB (42.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+    //method 1 sorting
+    /*
     bool isAnagram(string s, string t) {
         if (s.size()!=t.size()) return false;
         
@@ -8,4 +11,31 @@ public:
         
         return s==t;          
     }
+    */
+    
+    //method 2 by counting/hashing
+    
+       bool isAnagram(string s, string t) {
+       
+           int freqTable[256]={0};
+           
+           for(int i=0;i<s.size();i++){
+               freqTable[s[i]]++;
+           }
+           
+           for(int i=0;i<t.size();i++){
+               freqTable[t[i]]--;
+           }
+           
+           for(int i=0;i<256;i++){
+               if(freqTable[i] != 0){
+                   return false;
+               }
+           }
+           return true;
+           
+    }
+    
+    
+    
 };
