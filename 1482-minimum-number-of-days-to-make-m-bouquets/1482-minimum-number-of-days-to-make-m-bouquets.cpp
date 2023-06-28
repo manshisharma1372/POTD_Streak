fc6185e0d07f24392b616b73615e31d43c1e6c0b
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-06-28 23:31:44 +0530

    Time: 211 ms (54.48%), Space: 66.5 MB (57.04%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -17,17 +17,23 @@ public:
 	noOfB+=(cnt/k);
 	return noOfB>=m;
 }
+    
+    
+    
     int minDays(vector<int>& arr, int r, int b) {
         long long val= r*1LL*b*1LL;
 	if(val>arr.size()) return -1;
-	//int mini=*min_element(arr.begin(),arr.end());
-	///int maxi= *max_element(arr.begin(),arr.end());
+	int mini=*min_element(arr.begin(),arr.end());
+	int maxi= *max_element(arr.begin(),arr.end());
 	
+        
+        /*
 	int mini=INT_MAX,maxi=INT_MIN;
 	for(int i=0;i<arr.size();i++){
 		mini=min(mini,arr[i]);
 		maxi=max(maxi,arr[i]);
 	}
+    */
 	
 	int low=mini;
 	int high=maxi;
