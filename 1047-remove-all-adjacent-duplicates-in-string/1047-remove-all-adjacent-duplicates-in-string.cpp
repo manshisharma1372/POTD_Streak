4529477b7099d913ec2287c4ea772147f3235918
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-07-29 13:46:23 +0530

    Time: 18 ms (69.64%), Space: 11.5 MB (33.06%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -25,7 +25,7 @@ public:
         
         
         
-        
+        /*
         
         //@method 2
         
@@ -40,6 +40,33 @@ public:
                  }
            
         }
+        return ans;
+    }
+    */
+        
+      // @method 3 stack
+        
+     stack<char> st;
+        string ans;
+        for(int i=0;i<s.size();i++){
+            
+            char ch=s[i];
+            
+            if(!st.empty() && s[i]==st.top()){
+                st.pop();
+            }
+            
+            else{
+                st.push(ch);
+            }
+        }
+        
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 
