99c9dd0a736fa0fe325ec967b3bb2b22d2ac7204
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-09-05 16:06:35 +0530

    Added solution - LeetHub

--- Second Largest - GFG/second-largest.cpp
+++ Second Largest - GFG/second-largest.cpp
@@ -12,20 +12,19 @@ public:
 	int print2largest(int arr[], int n) {
 	    // code here
 	    
-	    int second_largest=-1;
 	    int largest=arr[0];
+	    int secondLargest=-1;
 	    
 	    for(int i=0;i<n;i++){
 	        if(arr[i]>largest){
-	            second_largest=largest;
+	            secondLargest=largest;
 	            largest=arr[i];
 	        }
-	        else if(arr[i]<largest && arr[i]>second_largest){
-	            second_largest=arr[i];
+	        else if(arr[i]<largest && arr[i]>secondLargest){
+	            secondLargest=arr[i];
 	        }
 	    }
-	    return second_largest;
-	
+	    return secondLargest;
 	}
 };
 
