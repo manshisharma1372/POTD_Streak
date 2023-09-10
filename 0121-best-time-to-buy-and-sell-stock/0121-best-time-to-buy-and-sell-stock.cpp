6d4b1679e0695822631cf12207151516dc70a0b0
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-09-10 22:19:34 +0530

    Time: 79 ms (97.19%), Space: 93.4 MB (44.33%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,93 +1,16 @@
 class Solution {
 public:
-    
-    
-    
-    
-    
-    
-    
-    void maxProfitFinder(vector<int> &prices,int i,int &minPrice,int &maxProfit){
-        
-        //base
-        if(i==prices.size()){
-            return;
-        }
-        
-        //ek case
-        if(prices[i]<minPrice){
-            minPrice=prices[i];
-        }
-        
-        int todaysprofit=prices[i]-minPrice;
-        
-        if(todaysprofit>maxProfit){
-            maxProfit=todaysprofit;
-        }
-        
-        //RE
-        maxProfitFinder(prices,i+1,minPrice,maxProfit);
-        
-    }
-
     int maxProfit(vector<int>& prices) {
         
+        int maxProfit=0;
+        int mini=prices[0];
+        int n=prices.size();
         
-        //o(n2) //gives TLE
-        /*
-        int ans=0;
-        
-        int i=0;
-        int j=i+1;
-        while(i<prices.size() && j<prices.size())
-        {
-            for(int j=i+1;j<prices.size();j++){
-            int p= prices[j]-prices[i];
-            ans=max(ans,p);
-            }
-            i++;
-
+        for(int i=1;i<n;i++){
+            int profit=prices[i]-mini;
+            maxProfit=max(maxProfit,profit);
+            mini=min(mini,prices[i]);
         }
-        return ans;
-        */
-        
-        
-        //iterative soln
-        /*
-        int lsf=INT_MAX;
-        int pist = 0;
-        int op=0;
-        
-        for(int i = 0; i < prices.size(); i++){
-            if(prices[i] < lsf){
-                lsf = prices[i];
-            }
-            pist = prices[i] - lsf;
-            if(op < pist){
-                op = pist;
-            }
-        }
-        return op;
-        */
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //recursive soln
-        
-        
-        int minPrice=INT_MAX;
-        int maxProfit=INT_MIN;
-        maxProfitFinder(prices,0,minPrice,maxProfit);
         return maxProfit;
     }
 };
