a0dc9dacd4fbf2da05ff044b557774a60ab8fafe
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-09-14 02:13:06 +0530

    Time: 9 ms (29.49%), Space: 7.7 MB (5.21%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -3,20 +3,26 @@ public:
     bool isIsomorphic(string s, string t) {
         
         map<char,char> m;
-        set<char> x;
+        set<char> st; 
+        
         for(int i=0;i<s.size();i++){
             
-            if(m.count(s[i])==0 && x.count(t[i])==0){
+            if(m.count(s[i])==0 && st.count(t[i])==0){
                 m[s[i]]=t[i];
-                x.insert(t[i]);
+                
+                st.insert(t[i]);
             }
             
-            else if( m.count(s[i])==0 && x.count(t[i])){
+            else if( m.count(s[i])==0 && st.count(s[i]) ){
                 return false;
             }
-            else if( m[s[i]] !=t[i]){
+            
+            else if( m[s[i]] != t[i]){
                 return false;
+                
             }
+            
+            
         }
         return true;
     }
