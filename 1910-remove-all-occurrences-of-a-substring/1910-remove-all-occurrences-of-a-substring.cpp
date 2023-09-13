10d2693859c6bfa9b8a88af5a63c4cc7011f33b8
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-09-13 19:01:07 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (13.60%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,59 +1,14 @@
 class Solution {
 public:
-    
-    void removeocc(string &s,string &part){
-        
-        int found=s.find(part);
-        if(found!=string::npos){
-            //remove part string
-            
-            string left=s.substr(0,found);
-            string right=s.substr(found+part.size(),s.size());
-            s=left+right;
-            removeocc(s,part);
-        }
-        
-        else{
-            
-            //all oc have been removed base case
-            return;
-        }
-        
-    }
     string removeOccurrences(string s, string part) {
         
-        
-        //iterative soln
-        /*
         int pos=s.find(part);
         while(pos!=string::npos){
-            
             s.erase(pos,part.length());
-            
             pos=s.find(part);
             
         }
+        
         return s;
-        */
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //recursive soln
-        removeocc(s,part);
-        return s;
-        
     }
 };
