e7702225b5c48eb106f85bc10a5a9b8d7b119035
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-09-14 00:39:01 +0530

    Time: 12 ms (37.65%), Space: 7.6 MB (6.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,41 +1,15 @@
 class Solution {
 public:
-    
-    //method 1 sorting
-    /*
     bool isAnagram(string s, string t) {
-        if (s.size()!=t.size()) return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
         
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        
-        return s==t;          
+        if(s.compare(t)==0){
+            return true;
+        }
+        else{
+               return false;
+        }
+     
     }
-    */
-    
-    //method 2 by counting/hashing
-    
-       bool isAnagram(string s, string t) {
-       
-           int freqTable[256]={0};
-           
-           for(int i=0;i<s.size();i++){
-               freqTable[s[i]]++;
-           }
-           
-           for(int i=0;i<t.size();i++){
-               freqTable[t[i]]--;
-           }
-           
-           for(int i=0;i<256;i++){
-               if(freqTable[i] != 0){
-                   return false;
-               }
-           }
-           return true;
-           
-    }
-    
-    
-    
 };
