0e6dbbeb81ab7e97ffaa5091f6ff75e498f8f8b5
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-09-13 20:07:14 +0530

    Time: 14 ms (43.95%), Space: 13.9 MB (9.50%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,52 +1,33 @@
 class Solution {
 public:
-    int findMinDifference(vector<string>& timePoints) {
+    int findMinDifference(vector<string>& timepoints) {
         
-        
-        //step 1 convert time string into minute integer val
         vector<int> minutes;
         
-        for(int i=0;i<timePoints.size();i++){
+        for(int i=0;i<timepoints.size();i++){
             
-            string curr=timePoints[i];
-            int hours=stoi(curr.substr(0,2));
+            string curr=timepoints[i];
+            int hr= stoi(curr.substr(0,2));
+            int minu= stoi(curr.substr(3,2));
             
-            int minu=stoi(curr.substr(3,2));
+            int totalmin= hr*60+minu;
             
-            int total_min=hours*60+minu;
+            minutes.push_back(totalmin);
             
-            minutes.push_back(total_min);
-         
         }
-            //step 2 sort
-            
-            sort(minutes.begin(),minutes.end());
-            
-            //step 3 cal difference and then cal min diff
-            
-           int mini_val=INT_MAX;
-            int n=minutes.size();
-            for(int i=0;i<n-1;i++)
-            {
-                int diff=minutes[i+1]-minutes[i];
-                
-                mini_val=min(mini_val,diff);
-            }
-           
         
-        //corner case
-        //1440 is 24 hr
-        int last_diff1=(minutes[0]+1440)-minutes[n-1];
+        sort(minutes.begin(),minutes.end());
         
-        int last_diff2=minutes[n-1]-minutes[0];
+        int mini_val=INT_MAX;
+        for(int i=0;i<minutes.size()-1;i++){
+            int diff=minutes[i+1]-minutes[i];
+            mini_val=min(mini_val,diff);
+        }
         
-        int last_diff=min(last_diff1,last_diff2);
+        int lastDiff=(minutes[0]+1440)-minutes[minutes.size()-1];
         
-        mini_val=min(mini_val,last_diff);
+        mini_val=min(mini_val,lastDiff);
         
-            
-        
-      return mini_val;
-        
+        return mini_val;
     }
 };
