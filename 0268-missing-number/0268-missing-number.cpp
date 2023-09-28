e2812d8a16efc4c068239c4833ccd62897d7e424
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-09-29 00:49:23 +0530

    Time: 7 ms (98.17%), Space: 18.4 MB (9.13%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
+    int missingNumber(vector<int>& a) {
+      
+    long long n=a.size();
+    
+    long long sn=(n*(n+1)/2);
+    long long s2n=(n*(n+1)*(2*n+1))/6;
+    long long s=0;
+    long long s2=0;
+    for(int i=0;i<n;i++){
+        s+=a[i];
+        s2+=(long long)a[i]* (long long)a[i];
         
-        int sum=0;
-        for(int i=0;i<=nums.size();i++){
-            sum+=i;
+    }
+    
+    long long val1=s-sn;
+    long long val2=s2-s2n;
+        if(val1!=0){
+               val2=val2/val1;
         }
-        
-        int arrSum=0;
-        for(int i=0;i<nums.size();i++){
-            arrSum+=nums[i];
-        }
-        
-        int missing=sum-arrSum;
-        return missing;
+ 
+    long long x=(val1+val2)/2;
+    long long y=x-val1;
+    
+    return (int)y;
+    
+
     }
 };
