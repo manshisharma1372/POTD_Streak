f895e84064c71713684b8e76614174cc8bcd6a10
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2023-11-07 02:11:28 +0530

    Added solution - LeetHub

---
+++ Subset Sums - GFG/subset-sums.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+
+  void findSubsetSum(int ind,int sum,vector<int>& arr,int n,vector<int> &ans){
+      
+      if(ind==n){
+          ans.push_back(sum);
+          return;
+      }
+      
+      //pick
+      findSubsetSum(ind+1,sum+arr[ind],arr,n,ans);
+      
+      //not pick
+      findSubsetSum(ind+1,sum,arr,n,ans);
+  }
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        // Write Your Code here
+        vector<int> ans;
+        int ind=0;
+        int sum=0;
+        findSubsetSum(ind,sum,arr,N,ans);
+        
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0 ; i < N ; i++){
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<int> ans = ob.subsetSums(arr,N);
+        sort(ans.begin(),ans.end());
+        for(auto sum : ans){
+            cout<< sum<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
